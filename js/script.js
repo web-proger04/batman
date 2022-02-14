bbb88f877ac9a414fa769f256d517e8b632cfb63
web-proger04 <92223429+web-proger04@users.noreply.github.com>
2022-02-15 01:46:39 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,4 +5,44 @@ const swiper = new Swiper('.batman-slider', {
   navigation: {
     nextEl: '.batman-slider__arrow',
   },
+  breakpoints: {
+  	320: {
+  		slidesPerView: 1,
+  	},
+  	768: {
+  		slidesPerView: 2,
+  	}
+  }
 });
+
+
+let headerBody = document.querySelector('.header__body');
+let headerBurger = document.querySelector('.header__burger');
+let headerMenuClose = document.querySelector('.header__menu-close')
+
+headerBurger.addEventListener('click', () => {
+	headerBody.style.display = 'flex';
+})
+
+headerMenuClose.addEventListener('click', () => {
+	headerBody.style.display = 'none';
+})
+
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.batman__button-play');
+
+buttonModal.addEventListener('click', (e) => {
+	modalWindow.classList.add('active');
+})
+
+modalWindow.addEventListener('click', (e) => {
+	const isModal = e.target.closest('.modal__inner');
+
+	if (!isModal) {
+	   modalWindow.classList.remove('active');
+	}
+});
+
+
+console.log(buttonModal);
